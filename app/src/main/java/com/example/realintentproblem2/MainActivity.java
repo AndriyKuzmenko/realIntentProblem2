4cfe134a48cd9f9f8764e40b3cc72c165f809430
Andriy <ak8022@bs.amalnet.k12.il>
2020-09-05 15:57:35 +0300

    Random numbers

--- app/src/main/java/com/example/realintentproblem2/MainActivity.java
+++ app/src/main/java/com/example/realintentproblem2/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.realintentproblem2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
 {
     EditText etA, etB, etC;
+    Random r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,5 +20,13 @@ public class MainActivity extends AppCompatActivity
         etA=(EditText)findViewById(R.id.etA);
         etB=(EditText)findViewById(R.id.etB);
         etC=(EditText)findViewById(R.id.etC);
+        r=new Random();
+    }
+
+    public void randomNumbers(View view)
+    {
+        etA.setText(""+r.nextDouble());
+        etB.setText(""+r.nextDouble());
+        etC.setText(""+r.nextDouble());
     }
 }
