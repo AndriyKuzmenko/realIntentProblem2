08170b2ab68d480cb1327c5f43bd395c1daf7eb1
Andriy <ak8022@bs.amalnet.k12.il>
2020-09-05 15:50:41 +0300

    Code part 1

--- app/src/main/java/com/example/realintentproblem2/MainActivity.java
+++ app/src/main/java/com/example/realintentproblem2/MainActivity.java
@@ -1,14 +1,22 @@
-package com.example.realintentproblem2;
+package com.example.intentproblem2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    EditText etA, etB, etC;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etA=(EditText)findViewById(R.id.etA);
+        etB=(EditText)findViewById(R.id.etB);
+        etC=(EditText)findViewById(R.id.etC);
     }
 }
