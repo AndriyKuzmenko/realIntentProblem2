02b3d162d526f4fe988963506a42f36b6a5d89cd
Andriy <ak8022@bs.amalnet.k12.il>
2020-09-05 17:30:44 +0300

    Drawing parabole

--- app/src/main/java/com/example/realintentproblem2/results.java
+++ app/src/main/java/com/example/realintentproblem2/results.java
@@ -29,6 +29,31 @@ public class results extends AppCompatActivity
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new myWV());
+
+        String equation="https://www.google.com/search?q=";
+        if (a!=0 && a!=1 && b!=0 && b!=1 && c!=0 && c!=1)
+        {
+            equation+=a + "x%5E2%2B" + b + "x%2B" + c + "&aqs=chrome.0.69i59j0l7.3454j0j9&sourceid=chrome&ie=UTF-8";
+            webView.loadUrl(equation);
+        }
+
+        if (a!=1)
+        {
+            equation+=a;
+        }
+        equation+="x%5E2%2B";
+
+        if (b==1)
+        {
+            equation+="x%2B";
+        }
+        else if (b!=0)
+        {
+            equation+=b+"x%2B";
+        }
+
+        equation+=c;
+        webView.loadUrl(equation);
     }
 
     private class myWV extends WebViewClient
