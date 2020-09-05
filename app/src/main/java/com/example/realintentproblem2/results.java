37881426e74e9abbfb080a6e3f62f4861a55e5f9
Andriy <ak8022@bs.amalnet.k12.il>
2020-09-05 17:18:33 +0300

    getting numbers

--- app/src/main/java/com/example/realintentproblem2/results.java
+++ app/src/main/java/com/example/realintentproblem2/results.java
@@ -2,15 +2,26 @@ package com.example.realintentproblem2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class results extends AppCompatActivity
 {
+    Intent gi;
+    double a,b,c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        gi=getIntent();
+        a=gi.getDoubleExtra("a",0);
+        b=gi.getDoubleExtra("a",0);
+        c=gi.getDoubleExtra("a",0);
+        //Because the code checks are the numbers correct
+        //before sending them, the default value doesn't
+        //matter.
     }
 }
